63eea002bd76bb69573fcd0b989a95d8a6b79f01
Himanshu <himanshu_196018@saitm.org>
2023-09-23 23:37:44 +0530

    test running

--- scripts/action.js
+++ scripts/action.js
@@ -1,10 +1,18 @@
-const subButton = document.getElementById('subButton');
-const feedbackCard = document.getElementById('feedback-card');
-const responseCard = document.getElementById('response-card');
-const ratingList = document.getElementsByClassName('num');
+const subButton = document.getElementById("subButton");
+const feedbackCard = document.getElementById("feedback-card");
+const responseCard = document.getElementById("response-card");
+const rating = document.getElementsByClassName("rating")[0].children;
+const ratingList = document.getElementsByClassName("num");
 
 function clickEvent() {
-    // console.log(feedbackCard);
-    feedbackCard.style.display = "none";
-    responseCard.style.display = "block";
+  // console.log(feedbackCard);
+  feedbackCard.style.display = "none";
+  responseCard.style.display = "block";
 }
+
+function customFun(e) {
+  ratingList[e].style.backgroundColor = "var(--light-grey)";
+  ratingList[e].style.color = "white";
+}
+
+console.log(rating);
