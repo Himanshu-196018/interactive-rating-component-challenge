b14f12674010274a155f1643611690531d25c940
Himanshu <himanshu_196018@saitm.org>
2023-09-23 23:20:44 +0530

    added submit action though JS

--- scripts/action.js
+++ scripts/action.js
@@ -1,1 +1,10 @@
-console.log("Hello, Thanks for Visiting my site");
+const subButton = document.getElementById('subButton');
+const feedbackCard = document.getElementById('feedback-card');
+const responseCard = document.getElementById('response-card');
+const ratingList = document.getElementsByClassName('num');
+
+function clickEvent() {
+    // console.log(feedbackCard);
+    feedbackCard.style.display = "none";
+    responseCard.style.display = "block";
+}
